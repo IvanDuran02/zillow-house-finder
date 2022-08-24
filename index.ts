7bcf54fa2f038b1fbd1502e6b730c1acd86bf784
Ivan Duran <iamivanduran02@gmail.com>
2022-08-24 12:31:26 -0400

    added locations to go through

--- index.ts
+++ index.ts
@@ -1,4 +1,21 @@
 import * as cheerio from "cheerio";
+import axios from "axios";
+
+const locations = [
+  "miami",
+  "pensacola",
+  "dallas",
+  "austin",
+  "houston",
+  "san antonio",
+  "palm-beach",
+  "las-vegas",
+  "las-angeles",
+  "san-fran",
+  "toronto",
+  "brampton",
+  "montreal",
+];
 
 const formatAddr = (data: any, i: number) => {
   // cut the undisclosed address from the address so that we can construct the image url
@@ -18,26 +35,30 @@ const formatAddr = (data: any, i: number) => {
   return formatUndisclosedAddress3;
 };
 
-async function getProperty() {
-  const response = await fetch("https://www.zillow.com/homes/palm-beach"); // fetch the page
-  const body = await response.text(); // get the body of the page (source HTML)
+async function getProperty(location: string) {
+  const url = `https://www.zillow.com/homes/${location}`;
+  const response = await axios(url); // fetch the page
+  const body = await response.data; // get the body of the page (source HTML)
   const $ = cheerio.load(body); // load the HTML into cheerio
   const data: any = [];
   const formatted: any = [];
   const links: any = [];
+  // const images: any = [];
+
+  console.log(body);
 
   $(".list-card").each((i, element) => {
     const address = $(element).find(".list-card-addr").text();
     const price = $(element).find(".list-card-price").text();
-    // const image = $(element).find("img").attr("src"); // wrong image link
     const zpid = $(element).attr("id");
 
     data.push({ address, price, zpid });
   });
   data.pop(); // remove the last element of the array, for some reason last element is empty
+
   for (let i = 0; i < data.length; i++) {
     if (data[i].address.startsWith("(undisclosed Address),")) {
-      formatted.push(formatAddr(data, i)); // format the undisclosed address
+      formatted.push(await formatAddr(data, i)); // format the undisclosed address
     } else {
       const formattedData = data[i].address.replace(/\s/g, "-"); // remove all whitespace
 
@@ -51,13 +72,24 @@ async function getProperty() {
     );
   }
 
-  data.forEach((item: any, index: number) => (item.link = links[index]));
-  console.log(data);
+  data.forEach(
+    (property: any, index: number) => (property.link = links[index])
+  ); // merges the links into the data array
 
-  // const formattedData3 = formattedData2.replace(",", ""); // remove all whitespace
-  // console.log(formattedData3);
+  data.forEach(async (property: any, index: number) => {
+    const fetchImageUrl = await fetch(property.link);
+    const imgBody = await fetchImageUrl.text(); // get the body of the page (source HTML)
+    const $$ = cheerio.load(imgBody); // load the HTML into cheerio
+    $$(".media-column-container")
+      .find("img")
+      .each((i, element) => {
+        console.log($(element).attr("src"));
+      });
+  });
+
+  // console.log(data);
 }
 
-getProperty();
-
-// const link = $(".list-card").attr("id"); -- gets the zpid so that we can go to property page and grab all the images
+for (let i = 0; i < locations.length; i++) {
+  getProperty(locations[i]);
+}
